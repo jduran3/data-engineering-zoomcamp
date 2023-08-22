b4a170e9a8e5e8bf470ce2c184599cfb39fb5394
jduran3 <joduran79@gmail.com>
2023-08-22 16:28:53 -0600

    changes

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='view') }}
 
-/*with tripdata as 
+with tripdata as 
 #(
 #  select *,
 #    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
 #  from {{ source('staging','green_tripdata') }}
 #  where vendorid is not null 
-#)*/
+#)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -40,13 +40,13 @@ select
 
 from {{ source('staging','green_tripdata') }}
 limit 100
---from tripdata
---where rn = 1
+from tripdata
+where rn = 1
 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-/*{% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=true) %}
 
   limit 100
 
-{% endif %}*/
+{% endif %}
